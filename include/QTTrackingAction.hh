ecab3853cdcd384f6c63c38fe886b5d1d502755e
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2023-02-13 08:41:24 +0000

    Remove argument from destructor

--- include/QTTrackingAction.hh
+++ include/QTTrackingAction.hh
@@ -2,13 +2,14 @@
 #define QTTrackingAction_h 1
 
 #include "G4UserTrackingAction.hh"
+#include "G4Types.hh"
 #include <vector>
 
 class QTTrackingAction : public G4UserTrackingAction
 {
 public:
   QTTrackingAction();
-  virtual ~QTTrackingAction(std::vector<G4double>&){};
+  virtual ~QTTrackingAction(){};
 
   virtual void PreUserTrackingAction(const G4Track*);
   virtual void PostUserTrackingAction(const G4Track*);
