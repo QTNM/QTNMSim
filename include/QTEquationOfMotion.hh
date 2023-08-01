95a5f96f8d9527af4ff00299a2c06fc678dae6cd
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2023-08-01 15:38:12 +0100

    [EoM] added radiation acceleration function, all in SI units.

--- include/QTEquationOfMotion.hh
+++ include/QTEquationOfMotion.hh
@@ -20,7 +20,7 @@ public:
 			     G4double mass);
 
   G4ThreeVector GetCachedFieldValue();
-  G4ThreeVector CalcRadiationAcceleration(G4ThreeVector, G4ThreeVector, G4ThreeVector);
+  G4ThreeVector CalcRadiationAcceleration(G4ThreeVector, G4ThreeVector);
   G4ThreeVector CalcOmegaGivenB(G4ThreeVector, G4ThreeVector);
   G4ThreeVector CalcAccGivenB(G4ThreeVector, G4ThreeVector);
 
@@ -30,8 +30,10 @@ private:
   G4double fCharge;
 
   // conversion in [kg] not working
-  static constexpr G4double m_e = 9.1093837e-31; // [electron mass in [kg]; needed only here
-  static constexpr G4double c_SI = c_light/(m/s); // explicit SI units
+  static constexpr G4double m_e     = 9.1093837e-31; // [electron mass in [kg]; needed only here
+  static constexpr G4double c_SI    = c_light/(m/s); // explicit SI units
+  static constexpr G4double eps0_SI = epsilon0/(farad/m); // explicit SI units
+  static constexpr G4double tau_SI  = e_SI*e_SI/(6.0*pi*eps0_SI*c_SI*c_SI*c_SI*m_e); // 6.26e-24 s
 
 };
 
