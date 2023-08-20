ac967097a896271853246d3000c6d24007c88312
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2023-08-20 12:08:49 +0100

    [tracking] add option to store trajectories. Add all command macro.

--- src/QTTrackingAction.cc
+++ src/QTTrackingAction.cc
@@ -15,9 +15,12 @@ QTTrackingAction::~QTTrackingAction() = default;
 
 void QTTrackingAction::PreUserTrackingAction(const G4Track* aTrack)
 {
-  // Create trajectory for track
-  fpTrackingManager->SetStoreTrajectory(true);
-  fpTrackingManager->SetTrajectory(new QTTrajectory(aTrack, angles));
+  // Create trajectory for track if requested
+  // fpTrackingManager->SetStoreTrajectory(true);
+  if(fpTrackingManager->GetStoreTrajectory() > 0)
+  {
+    fpTrackingManager->SetTrajectory(new QTTrajectory(aTrack, angles));
+  }
 }
 
 void QTTrackingAction::PostUserTrackingAction(const G4Track* aTrack)
