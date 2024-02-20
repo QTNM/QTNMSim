1ec0c41a9cb362368edde7bfb42b84ebe9ca96b7
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2024-02-20 10:39:19 +0000

    [readme] update readme and test remove condition on scoring.

--- src/QTGasSD.cc
+++ src/QTGasSD.cc
@@ -40,9 +40,9 @@ G4bool QTGasSD::ProcessHits(G4Step* aStep,
   if (vprocess->GetProcessName()=="Transportation") return false;
 
   // pitch angle change condition < 0.057 degrees
-  G4ThreeVector premom  = aStep->GetPreStepPoint()->GetMomentumDirection();
-  G4ThreeVector postmom = aStep->GetPostStepPoint()->GetMomentumDirection();
-  if (fabs(premom.theta()-postmom.theta()) < 1.0e-3) return false;
+  // G4ThreeVector premom  = aStep->GetPreStepPoint()->GetMomentumDirection();
+  // G4ThreeVector postmom = aStep->GetPostStepPoint()->GetMomentumDirection();
+  // if (fabs(premom.theta()-postmom.theta()) < 1.0e-3) return false;
   
   QTGasHit* newHit = new QTGasHit();
   G4ThreeVector preloc = aStep->GetPreStepPoint()->GetPosition();
