13bdd8a323ce180b40139211eab9adab61829ad1
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2024-08-12 22:25:24 +0100

    New, simplified DetectorConstruction.
    Compiles, untested.

--- examples/scattering.cc
+++ examples/scattering.cc
@@ -21,13 +21,12 @@
 #include "G4Threading.hh"
 #include "G4GenericPhysicsList.hh"
 #include "G4VModularPhysicsList.hh"
-#include "G4GDMLParser.hh"
 #include "G4LogicalVolume.hh"
 #include "G4StepLimiterPhysics.hh"
 
 // us
 #include "CLI11.hpp"  // c++17 safe; https://github.com/CLIUtils/CLI11
-#include "QTDetectorConstruction.hh"
+#include "SCDetectorConstruction.hh"
 #include "QTActionInitialization.hh"
 
 int main(int argc, char** argv)
@@ -38,11 +37,9 @@ int main(int argc, char** argv)
   int         seed     = 1234;
   std::string outputFileName("qtnm.root");
   std::string macroName;
-  std::string gdmlFileName("test.gdml");
   std::string physListName("QTNMPhysicsList");
 
   app.add_option("-m,--macro", macroName, "<Geant4 macro filename> Default: None");
-  app.add_option("-g,--gdml", gdmlFileName, "<Geant4 GDML filename> Default: test.gdml");
   app.add_option("-p,--physlist", physListName, "<Geant4 physics list macro> Default: QTNMPhysicsList");
   app.add_option("-s,--seed", seed, "<Geant4 random number seed + offset 1234> Default: 1234");
   app.add_option("-o,--outputFile", outputFileName,
@@ -77,33 +74,8 @@ int main(int argc, char** argv)
   runManager->SetNumberOfThreads(nthreads);
 #endif
 
-
-  G4GDMLParser parser;
-  parser.SetOverlapCheck(true);
-  parser.Read(gdmlFileName); // const G4String&
-
-  // retrieve antenna information
-  std::vector<double> angles;
-  const G4GDMLAuxMapType* auxmap = parser.GetAuxMap();
-  for(G4GDMLAuxMapType::const_iterator iter=auxmap->begin();
-      iter!=auxmap->end(); iter++)
-    {
-      G4LogicalVolume* lv = (*iter).first;
-      G4String nam = lv->GetName();
-      // in name of logical volume, even for CAD input, assume 'Antenna'
-      // since that should receive a list of auxiliaries.
-      if (G4StrUtil::contains(nam, "Antenna")) {
-	for (auto entry : (*iter).second) { // G4GDMLAuxStructType in std::vector
-	  if (entry.type=="angle") { // assume radians
-	    std::string theta = entry.value;
-	    angles.push_back(std::stod(theta)); // convert string to double
-	  }
-	}
-      }
-    }
-
   // -- Set mandatory initialization classes
-  runManager->SetUserInitialization(new QTDetectorConstruction(parser));
+  runManager->SetUserInitialization(new SCDetectorConstruction());
 
   // -- set user physics list
   // Physics list factory
@@ -122,8 +94,8 @@ int main(int argc, char** argv)
 
 
   // -- Set user action initialization class.
-  auto* actions = new QTActionInitialization(outputFileName, angles);
-  runManager->SetUserInitialization(actions);
+  //auto* actions = new QTActionInitialization(outputFileName, angles);
+  //runManager->SetUserInitialization(actions);
 
 
   // Batch mode only - no visualisation
