9a2d1b8c72829dc07795738b5725730db7303271
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2025-03-10 15:41:15 +0000

    SetPolarAngleLimit(0.0) for elastic scattering in Multi-scattering

--- test/Test0/src/MultipleScattering.cc
+++ test/Test0/src/MultipleScattering.cc
@@ -109,7 +109,9 @@ void MultipleScatteringList::ConstructProcess() {
   G4CoulombScattering* ss = new G4CoulombScattering();
   G4EmMultiModel* mm = new G4EmMultiModel("CoulombSSModels");
   // Elastic Scattering
-  mm->AddModel(new G4eDPWACoulombScatteringModel(false, false, 0.0));
+  G4eDPWACoulombScatteringModel* es = new G4eDPWACoulombScatteringModel(false, false, 0.0);
+  es->SetPolarAngleLimit(0.0); // No mixed model
+  mm->AddModel(es);
   // Impact Ionisation
   mm->AddModel(new QTNMeImpactIonisation());
   ss->AddEmModel(0, mm);
