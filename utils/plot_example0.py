1a6f31aaf8fc1e14b51806fb10093c1ce500895c
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2026-04-20 12:25:25 +0100

    Allow user to specify output file for example0

--- utils/plot_example0.py
+++ utils/plot_example0.py
@@ -1,9 +1,27 @@
+#!/usr/bin/env python3
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import ROOT
 
 
-def plot_signal():
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="""Plotting script for example0
+        """,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("--output",
+                        default="example0.png",
+                        help="Filename to save plot to.",
+                        type=str,
+                        nargs="?"
+                        )
+
+    return parser.parse_args()
+
+def plot_signal(output_file="example0.png"):
     f = ROOT.TFile('qtnm.root')
     t = f.Get('ntuple/Signal')
     t.GetEntry(0)
@@ -17,8 +35,9 @@ def plot_signal():
     plt.ylabel('Voltage')
     plt.xlim(left=0)
     plt.tight_layout()
-    plt.savefig('example0.png')
+    plt.savefig(output_file)
 
 
 if __name__ == "__main__":
-    plot_signal()
+    options = parse_arguments()
+    plot_signal(options.output)
